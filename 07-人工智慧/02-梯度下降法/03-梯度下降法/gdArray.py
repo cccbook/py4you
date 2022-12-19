01217a92e97014408b3f3bbcdea75550e702dae3
ccckmit <ccckmit@gmail.com>
2022-12-19 11:58:28 +0800

    reorganize

--- 07-人工智慧/02-梯度下降法/03-梯度下降法/gdArray.py
+++ 07-人工智慧/02-梯度下降法/03-梯度下降法/gdArray.py
@@ -1,9 +1,8 @@
-import gd2 as gd
+import gd as gd
 
 def f(p):
     [x, y, z] = p
-    return (x-1.358)**2+(y-2.122)**2+(z-3.737)**2
-    # return (x-1)**2+(y-2)**2+(z-3)**2
+    return (x-1)**2+(y-2)**2+(z-3)**2
     # return (x-2)**2+3*(y-0.5)**2+(z-2.5)**2
     # return x*x + 3*y*y + z*z - 4*x - 3*y - 5*z + 8
 
